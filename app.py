ff5955b52469de4fab791e7de18bd04fb5d7f120
Tristan <tristandelrosal@gmail.com>
2025-01-13 20:06:20 +0100

    subida epica

--- app.py
+++ app.py
@@ -7,9 +7,41 @@ from pokemontcgsdk import Supertype
 from pokemontcgsdk import Subtype
 from pokemontcgsdk import Rarity
 import streamlit as st
+import numpy as np
+from tensorflow.keras.models import load_model
+from PIL import Image
+import pandas as pd
 
 # Load environment variables from .env file
 load_dotenv()
+
+# Cargar el modelo
+model_path = 'model/pokemon_card_predictor.keras'
+if not os.path.exists(model_path):
+    st.error(f"The model file '{model_path}' does not exist.")
+else:
+    try:
+        model = load_model(model_path)
+    except ValueError as e:
+        st.error(f"Error loading model: {e}")
+
+# Función para cargar y procesar una imagen
+def load_and_preprocess_image(image_path, image_size=(128, 128)):
+    image = Image.open(image_path)
+    image = image.resize(image_size)
+    image = np.array(image)
+    if image.shape[-1] == 4:  # Si la imagen tiene un canal alfa, eliminarlo
+        image = image[..., :3]
+    image = image / 255.0  # Normalizar la imagen
+    return image
+
+# Función para predecir el ID de una carta
+def predict_card_id(image_path, model, image_size=(128, 128)):
+    image = load_and_preprocess_image(image_path, image_size)
+    image = np.expand_dims(image, axis=0)  # Añadir una dimensión para el batch
+    predictions = model.predict(image)
+    predicted_class = np.argmax(predictions, axis=1)
+    return predicted_class[0]
 
 # Set the API key for the Pokémon TCG SDK
 api_key = os.getenv('POKEMONTCG_IO_API_KEY')
@@ -19,8 +51,29 @@ if api_key:
 # Set up the Streamlit app
 st.title("Pokémon Card Finder")
 
-# Input for card ID
-card_id = st.text_input("Enter the Pokémon Card ID:")
+if __name__ == "__main__":
+    # Cargar el DataFrame para obtener el mapeo de IDs
+    df = pd.read_csv('./data/cards_with_variations.csv')
+    id_to_label = {i: label for i, label in enumerate(df['id'].astype('category').cat.categories)}
+
+    # Probar el modelo con una nueva imagen
+    test_image_path = './base3-2_original.png'  # Reemplazar con la ruta de la imagen
+    predicted_class = predict_card_id(test_image_path, model)
+    predicted_label = id_to_label[predicted_class]
+    st.write(f'Predicted Label: {predicted_label}')
+
+# Input for uploading an image
+uploaded_image = st.file_uploader("Sube una imagen de tu carta pokemon", type=["png", "jpg", "jpeg"])
+
+if uploaded_image is not None:
+
+    image = load_and_preprocess_image(uploaded_image)
+    predicted_class = predict_card_id(image, model)
+    predicted_label = id_to_label[predicted_class]
+    st.write(f'Predicted Label: {predicted_label}')
+    
+    card_id = predicted_label
+
 
 # Fetch and display the card details
 if card_id:
@@ -53,49 +106,4 @@ if card_id:
             st.write("Market price not available.")
     except Exception as e:
         st.error(f"Error fetching card: {e}")
-        
-# Ejemplo de uso del modelo
-import numpy as np
-from tensorflow.keras.models import load_model
-from PIL import Image
-import pandas as pd
-
-# Cargar el modelo
-model_path = 'model/pokemon_card_predictor.keras'
-if not os.path.exists(model_path):
-    st.error(f"The model file '{model_path}' does not exist.")
-else:
-    try:
-        model = load_model(model_path)
-    except ValueError as e:
-        st.error(f"Error loading model: {e}")
-
-# Función para cargar y procesar una imagen
-def load_and_preprocess_image(image_path, image_size=(128, 128)):
-    image = Image.open(image_path)
-    image = image.resize(image_size)
-    image = np.array(image)
-    if image.shape[-1] == 4:  # Si la imagen tiene un canal alfa, eliminarlo
-        image = image[..., :3]
-    image = image / 255.0  # Normalizar la imagen
-    return image
-
-# Función para predecir el ID de una carta
-def predict_card_id(image_path, model, image_size=(128, 128)):
-    image = load_and_preprocess_image(image_path, image_size)
-    image = np.expand_dims(image, axis=0)  # Añadir una dimensión para el batch
-    predictions = model.predict(image)
-    predicted_class = np.argmax(predictions, axis=1)
-    return predicted_class[0]
-
-if __name__ == "__main__":
-    # Cargar el DataFrame para obtener el mapeo de IDs
-    df = pd.read_csv('./data/cards_with_variations.csv')
-    id_to_label = {i: label for i, label in enumerate(df['id'].astype('category').cat.categories)}
-
-    # Probar el modelo con una nueva imagen
-    test_image_path = './base3-2_original.png'  # Reemplazar con la ruta de la imagen
-    predicted_class = predict_card_id(test_image_path, model)
-    predicted_label = id_to_label[predicted_class]
-    st.write(f'Predicted Class ID: {predicted_class}')
-    st.write(f'Predicted Label: {predicted_label}')
+    
